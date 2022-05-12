2b123b451fa351be146711cc3cbd991aad5229ba
Md Asaduzaman <asadasad9282@gmail.com>
2022-05-12 20:11:05 +0600

    Create Login Form using React Hook Form Validation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,26 +1,108 @@
 import React from 'react';
 import auth from '../../firebase.init';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { appendErrors, useForm } from 'react-hook-form';
 
 
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 
+    const { register, formState: { errors }, handleSubmit } = useForm();
 
+    if (user) {
+        console.log(user);
+    }
 
-
-
-
-
+    const onSubmit = data => {
+        console.log(data)
+    };
 
 
 
     return (
+
         <div className='flex h-screen justify-center items-center'>
             <div className="card w-96 bg-base-100 shadow-lg">
                 <div className="card-body ">
                     <h2 className="text-center font-bold text-2xl">Login</h2>
+
+
+                    {/*  form  */}
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Your Email</span>
+                            </label>
+
+                            {/* email input */}
+
+                            <input
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: 'email is required'
+                                    },
+
+                                    pattern: {
+                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                        message: "Please provide a valid Email"
+                                    }
+                                })}
+
+                                type="email"
+                                placeholder="Email"
+                                class="input input-bordered w-full max-w-xs"
+                            />
+                            <label class="label">
+                                {errors.email?.type === 'required' && <span className='label-text-alt text-red-500'>{errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span className='label-text-alt text-red-500'>{errors.email.message}</span>}
+                            </label>
+
+                        </div>
+
+                        {/* pasword  input*/}
+
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Password</span>
+
+                            </label>
+
+                            <input
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: 'password is required'
+                                    },
+
+                                    minLength: {
+                                        value: 6,
+                                        message: "Must be 6 characters or longer"
+                                    }
+                                })}
+
+                                type="password"
+                                placeholder="password"
+                                class="input input-bordered w-full max-w-xs"
+                            />
+                            <label class="label">
+                                {errors.password?.type === 'required' && <apan className='label-text-alt text-red-500'>{errors.password.message}</apan>}
+                                {errors.password?.type === 'minLength' && <apan className='label-text-alt text-red-500'>{errors.password.message}</apan>}
+                            </label>
+
+                        </div>
+
+
+
+                        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+                        <input type="number" {...register("age", { min: 18, max: 99 })} />
+                        <input type="submit" value={'LOGIN'} className='btn w-full text-white' />
+                    </form>
+
+
 
 
                     <div className="divider">OR</div>
