f1164ecbcbc6dd50261368a7cca86dd784ed5cce
Md Asaduzaman <asadasad9282@gmail.com>
2022-05-16 19:51:28 +0600

    lode all user admin page and create uadmin

---
+++ src/Pages/Dashbord/UserRow.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const UserRow = ({ user, refetch }) => {
+    const { email, role } = user
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')} `
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                refetch()
+                toast(`SuccessFulle made an admin`)
+            })
+    }
+    return (
+        <tr>
+            <th>{1}</th>
+            <td>{email}</td>
+            <td>{role !== 'admin' && <button onClick={makeAdmin} className='btn btn-xs'>Make Admine</button>}</td>
+            <td><button className='btn btn-xs'>Delete</button></td>
+            <td>{ }</td>
+        </tr>
+    )
+
+
+};
+
+export default UserRow;
--- src/Pages/Dashbord/Users.js
+++ src/Pages/Dashbord/Users.js
@@ -1,9 +1,51 @@
+import { keyboard } from '@testing-library/user-event/dist/keyboard';
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
+import UserRow from './UserRow';
 
 const Users = () => {
+
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: "GET",
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-            <h2>All users</h2>
+            <h2>All users {users?.length}</h2>
+
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>treetyment</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            users?.map(user => <UserRow
+                                user={user}
+                                key={user._id}
+                                refetch={refetch}
+                            ></UserRow>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
